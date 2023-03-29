eedca79c122f55eeee0f63c0eeec396b86ad6ba1
Chaeyoung Lim <rnldyal1027@naver.com>
2023-03-29 13:18:26 +0900

    commit

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -3,6 +3,7 @@ package kr.hs.study;
 import kr.hs.study.beans.TestBean1;
 import kr.hs.study.beans.TestBean2;
 import kr.hs.study.beans.TestBean3;
+import kr.hs.study.beans.TestBean4;
 import kr.hs.study.config.BeanConfigClass;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -21,14 +22,19 @@ public class Main {
         TestBean1 obj1 = ctx2.getBean("java1", TestBean1.class);
 //        TestBean2 obj2 = ctx2.getBean("java2", TestBean2.class);  //이름을 a로 바꿨기 때문에 존재하지 않는다고 에러가 난다.
 //        obj2.pr_Method();
-        TestBean2 obj3 = ctx2.getBean("a", TestBean2.class);
-        obj3.pr_Method();
-        System.out.println("obj3 : "+obj3);
+        //TestBean2 obj3 = ctx2.getBean("a", TestBean2.class);
+        //obj3.pr_Method();
+        //System.out.println("obj3 : "+obj3);
 //        TestBean3 obj4 = ctx2.getBean("java3", TestBean3.class);
 //        obj4.pr_Method2();
+        //TestBean2 obj5 = ctx2.getBean("a", TestBean2.class);
+        //System.out.println("obj5 : "+obj5);
 
-        TestBean2 obj5 = ctx2.getBean("a", TestBean2.class);
-        System.out.println("obj5 : "+obj5);
+        TestBean4 obj6 = ctx2.getBean("t4", TestBean4.class);
+        System.out.println("obj6 : "+obj6);
+
+        TestBean4 obj7 = ctx2.getBean("t5", TestBean4.class);
+        System.out.println("obj7 : "+obj7);
 
         ctx2.close();
     }
